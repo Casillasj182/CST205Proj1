865397e941a52a3693a2b0a26aa0874efe43b46a
Joel Casillas <jcasillas@csumb.edu>
2017-02-07 21:17:06 +0000

    Firdt FD

--- project1.py
+++ project1.py
@@ -1,7 +1,10 @@
+#Joel Casillas
+
+import statistics #imports a library for the median function
 from PIL import Image
 
-img = Image.open("Project1Images//1.png")
-img2 = Image.open("Project1Images//2.png")
+img1 = Image.open("Project1Images//1.png") # imported all the images from pil to python to open and use
+img2 = Image.open("Project1Images//2.png") 
 img3 = Image.open("Project1Images//3.png")
 img4 = Image.open("Project1Images//4.png")
 img5 = Image.open("Project1Images//5.png")
@@ -10,33 +13,55 @@ img7 = Image.open("Project1Images//7.png")
 img8 = Image.open("Project1Images//8.png")
 img9 = Image.open("Project1Images//9.png")
 
-print("This is the image size", img.size)
+print("Opened Files...")
 
-pictureWidth = 495
-pictureHeight = 557
-#pixels
-redmpixel=0
+
+#Prints out the image size so i can use for blank and to loop through
+
+Width = img1.size[0]
+Height = img1.size[1]
+print("Width", Width,"Height",Height)
+
+
+redmpixel=0 #Setting the pixels to default 0
 greenmpixel=0
 bluempixel=0
 
-#create lists for pixels
-redpixelList={}
-greenpixelList={}
-bluepixelList={}
 
-#create a new blank image with width and height of original
-newImage = Image.new("RGB",(495,557),"white")
+redPixelList=[]   #lists for the imgages Red,green and blue
+greenPixelList=[]
+bluePixelList=[]
 
-#store images in a list
-imageList = [img,img2,img3,img4,img5,img6,img7,img8,img9]
+newImage = Image.new("RGB",(Width,Height),"white") #creating a newimage that will be blank and itll be the size of the pic
 
-for x in range(0,pictureWidth):
-    for y in range(0,pictureHeight):
-        for image in ImageList:
-            redpixelList = redmpixel
+
+imageList = [img1,img2,img3,img4,img5,img6,img7,img8,img9] #Made a list to store all the 9 images in it
+
+for x in range(Width):       ##for loop to go through the pixels in the pic width, and range to 
+    for y in range(Height):       ##for loop to go through the pixels in the pic height
+        for Myimage in imageList:
             
-
-
+            myRed, myGreen, myBlue = Myimage.getpixel((x,y)) # x and y will go through each pixel and through all 9 images
+            
+            redPixelList.append(myRed)
+            greenPixelList.append(myGreen)
+            bluePixelList.append(myBlue)
+        redmpixel = statistics.median(redPixelList)
+        greenmpixel=statistics.median(greenPixelList)
+        bluempixel=statistics.median(bluePixelList)
+        redPixelList=[]   #lists for the imgages Red,green and blue
+        greenPixelList=[]
+        bluePixelList=[]
+        
+        newImage.putpixel((x,y),(redmpixel,greenmpixel,bluempixel)) 
+        
+     
+          
+               
+newImage.save('New image.jpg')           
+            
+            
+            
 
 
 
